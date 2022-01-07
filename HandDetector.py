549a7f8d9b40d656c2695ca5a02dc92546560375
Darshan62 <dmaj1961319@gmail.com>
2022-01-07 11:10:48 +0530

    updated handDetector

--- HandDetector.py
+++ HandDetector.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 class HandDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
         landmark_list = None
         self.mode = mode
         self.maxHands = maxHands
@@ -14,22 +14,22 @@ class HandDetector():
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, model_complexity=1,
-                                        min_detection_confidence=self.detectionCon,
-                                        min_tracking_confidence=self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands, model_complexity = 1, 
+                                        min_detection_confidence = self.detectionCon, 
+                                        min_tracking_confidence = self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def find_hands(self, img, draw=True):
+    def find_hands(self, img, draw = True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.result = self.hands.process(imgRGB)
         if self.result.multi_hand_landmarks:
             for handLms in self.result.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
+                    self.mpDraw.draw_landmarks(img, handLms, 
                                                self.mpHands.HAND_CONNECTIONS)
         return img
 
-    def find_position(self, img, handNo=0, draw=True):
+    def find_position(self, img, handNo = 0, draw = True):
 
         self.landmark_List = []
         if self.result.multi_hand_landmarks:
@@ -39,17 +39,17 @@ class HandDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 self.landmark_List.append([id, cx, cy])
                 if draw:
-                    self.mpDraw.draw_landmarks(img, single_Hand,
+                    self.mpDraw.draw_landmarks(img, single_Hand, 
                                                self.mpHands.HAND_CONNECTIONS)
 
         return self.landmark_List
 
     def finger_up(self, landmark_list):
 
-        self.fingers = [(landmark_list[8][2], landmark_list[7][2], 0),
-                   (landmark_list[12][2], landmark_list[11][2], 1),
-                   (landmark_list[16][2], landmark_list[15][2], 2),
-                   (landmark_list[20][2], landmark_list[19][2], 3),
+        self.fingers = [(landmark_list[8][2], landmark_list[7][2], 0), 
+                   (landmark_list[12][2], landmark_list[11][2], 1), 
+                   (landmark_list[16][2], landmark_list[15][2], 2), 
+                   (landmark_list[20][2], landmark_list[19][2], 3), 
                    (landmark_list[5][1], landmark_list[4][1], 4)]
 
         self.fingerup = [0, 0, 0, 0, 0]
